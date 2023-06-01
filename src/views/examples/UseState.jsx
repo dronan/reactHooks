9878b1f1890cf49bba443ae6905f9c821a6b6630
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-01 18:46:24 +0200

    exemplos de uso de useState

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+
+    const [count, setCount] = useState(0) // => [variavel, funcao], array destructuring array[0] e array[1]
+    const [name, setName] = useState("")
 
     return (
         <div className="UseState">
@@ -9,6 +13,25 @@ const UseState = (props) => {
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <SectionTitle title="Exercício #01" />
+                <div className="center">
+                    <span className="text">{count}</span>
+                    <div>
+                        <button className="btn" onClick={ _ => setCount(count - 1)}>-</button>
+                        <button className="btn" onClick={ _ => setCount(count + 1)}>+</button>
+                        <button className="btn" onClick={ _ => setCount(current => current + 1000)}>+1000</button>
+                    </div>
+                </div>
+
+            <SectionTitle title="Exercício #02" />
+                <div className="center">
+                    <div>
+                        <input type="text" className="input" value={name} onChange={e => setName(e.target.value)}/>
+                    </div>
+                    <span className="text">{name}</span>
+                </div>
+
         </div>
     )
 }
